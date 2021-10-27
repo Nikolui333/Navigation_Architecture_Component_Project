a7ef46cf0a1593af988f4f86ed692762a0d7e01f
Nikolui333 <semenovnikolu@yandex.ru>
2021-10-27 22:09:37 +0300

    fragment

---
+++ app/src/main/java/com/example/navigation_architecture_component_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.navigation_architecture_component_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
